3410fb1406ee162db5f9f9363dacdac17e754c8b
bharatgavade <68434631+bharatgavade@users.noreply.github.com>
2020-07-23 12:13:00 +0530

    DailyCodes: 18-july-2020

---
+++ july18/BitwiseDemo.java
@@ -0,0 +1,14 @@
+class BitwiseDemo {
+
+	public static void main(String[] args){
+
+	int x=9;
+	int y=10;
+	int ans;
+
+	ans=x&y;
+
+	System.out.println(ans);
+	}	
+
+}
---
+++ july18/BitwiseDemo3.java
@@ -0,0 +1,9 @@
+class BitwiseDemo {
+
+	public static void main(String[] args){
+	
+		int x=5;
+		System.out.println(~x);
+
+	}
+}
