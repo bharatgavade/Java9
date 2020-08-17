a2ba29a94bfb0deba54db60e4c088bc2ec0fd2f1
bharatgavade <68434631+bharatgavade@users.noreply.github.com>
2020-08-17 17:05:15 +0530

    Program12.java

--- Combine_Assignments/Combine_Assignment_01_14 Aug 2020/Program12.java
+++ Combine_Assignments/Combine_Assignment_01_14 Aug 2020/Program12.java
@@ -11,7 +11,9 @@ class Pro12{
         
         int i = 5; int j = 10; int k = 15; boolean ans;
         
-        ans = (i < (j - 10))? true: false;
-        System.out.println(ans);
+                System.out.println(ans=(i<(j-10)) ? true:false);
+		System.out.println(ans=(i==(i=j)) ? true:false);
+		System.out.println(ans=((i & j & k)!=0) ? true:false);
+		System.out.println(ans=((i ^ j >> 2)==0) ? true:false);
         }
 }
