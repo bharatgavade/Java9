357427d11b85c3b106bffa0aa283846bbdef7f77
bharatgavade <68434631+bharatgavade@users.noreply.github.com>
2020-08-28 22:03:40 +0530

    DailyFlash : 26-Aug-2020

---
+++ DailyFlash/26-Aug/prog2.java
@@ -0,0 +1,10 @@
+
+
+class Program {
+
+	public static void main(String[] args) {
+	
+		for(int i = 1; i <= 20; i++)
+			System.out.println("Cube of " + i + " : " + i*i*i);
+	}
+}
---
+++ DailyFlash/26-Aug/prog3.java
@@ -0,0 +1,11 @@
+
+
+class Program {
+
+	public static void main(String[] args) {
+	
+		for(int i = 1; i <= 10; i++)
+			System.out.print(i*2 + " ");
+		System.out.println();
+	}
+}
